12fa7addbb767e2f203a60e24488910999448abd
irshad <irshadiyas2979@gmail.com>
2024-07-08 16:21:06 +0400

    Add files via upload

---
+++ day09.js
@@ -0,0 +1,54 @@
+let lenght = 100;
+let width = 100;
+    console.log( "The area of a rectangle is: " , lenght * width);  
+
+
+let string1 = " Mohammed";
+let string2 = " irshad";
+
+let result = string1 + string2;
+
+    console.log("Concatenated string:", result);
+
+
+let num1 = "13520"
+let num2 = "57498"
+
+    if (num1 === num2) {
+       console.log(`${num1} and ${num2} are equal.`);
+  }   
+     else {
+       console.log(`${num1} and ${num2} are not equal.`);
+   }
+
+let number = 21;
+ 
+    if (number % 2 == 0) {
+        console.log("it's even");
+    } else {
+        console.log("it's odd");
+    }
+    
+let purchaseamount = 3760;
+
+     if (purchaseamount < 1000) {
+        console.log(purchaseamount - 0.1 * purchaseamount)
+     }
+     else if (purchaseamount > 1000 && purchaseamount < 5000) {
+        console.log(purchaseamount - 0.2 * purchaseamount)
+     }
+     else 
+        console.log(purchaseamount - 0.3 * purchaseamount)
+
+
+let age = 58;
+let price = 20;
+
+   if (age < 18) 
+        console.log("price = 10") ;
+     else if (age >= 18 && age <= 60) 
+        console.log("price = 20");
+ else 
+    console.log("price = 15");
+  
+    
